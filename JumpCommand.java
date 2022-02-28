af1aa226d6d746f504cfaed32492e54181a0115e
danielhenington <danhenington@gmail.com>
2022-02-27 19:23:22 -0500

    implemented JumpCommand and jump player method

--- JumpCommand.java
+++ JumpCommand.java
@@ -1,13 +1,21 @@
-
+/**
+ * @author Daniel Henington
+ * The command for making the character jump
+ */
 public class JumpCommand implements Command {
-   
-
+    private Player player;
+    /**
+     * Sets the player object to it's expected value.
+     * @param player 
+     */
     public JumpCommand(Player player) {
-        
+        this.player = player;
     }
-
+    /**
+     * Executes the jump command.
+     */
     public void execute() {
-        
+        player.jump();
     }
     
 }
--- Player.java
+++ Player.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Player {
 
     public Player() {
         System.out.println("Our hero is born.");
     }
-
+    /**
+     * Displays the character jumping up and down in the console
+     */
     public void jump() {
         /* Loop through the jump.txt file, 
         display 6 lines of the file at a time,  
@@ -11,6 +18,24 @@ public class Player {
         then clear the console
         Continue to do this until the file is empty
         */
+        File jump = new File("jump.txt");
+        Scanner scan = null;
+        try{
+            scan = new Scanner(jump);
+            while(scan.hasNextLine()) {
+                for(int i = 0; i < 6; i++) {
+                    System.out.println(scan.nextLine());
+                }
+                Thread.sleep(100);
+                System.out.print("\033[H\033[2J"); 
+                System.out.flush();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void fire() {
